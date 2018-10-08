514268436c61d00c30361d1ae5d0c93836a8c261
ecoulson <evan.m.coulson@gmail.com>
2018-10-07 23:40:48 -0700

    added some styling to the emissions tile

--- src/Components/App/Dashboard/Tiles/CarRow/EmissionTile/index.js
+++ src/Components/App/Dashboard/Tiles/CarRow/EmissionTile/index.js
@@ -6,7 +6,7 @@ import "./EmissionTile.css";
 export default class EmissionTile extends Component {
 	render() {
 		return (
-			<DashboardTile width={400} title="Daily C02 Emission From Car">
+			<DashboardTile width={400} title="Daily C02 Emission [Car]">
 				<h1>102k</h1>  {/*refactor too emission calculator*/}
 				<h4 className="emission-label">Tons Of Carbon</h4>
 			</DashboardTile>
